d965325443b31b550e1e93a7c1f73ad3f3b1b33e
awesomecurtis <88251469+awesomecurtis@users.noreply.github.com>
2024-01-23 12:00:08 +0000

    mainCard done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:currency_converter/currency_converter_cupertino_page.dart';
-import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
+// import 'package:currency_converter/currency_converter_cupertino_page.dart';
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-// import 'currency_converter_material_page.dart';
+import 'currency_converter_material_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,11 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return const MaterialApp(
-    return const CupertinoApp(
-      home:
-          // CurrencyConverterMaterialPage(),
-          CurrencyConverterCupertinoPage(),
+    return const MaterialApp(
+      // return const CupertinoApp(
+      home: CurrencyConverterMaterialPage(),
+      // CurrencyConverterCupertinoPage(),
     );
   }
 }
